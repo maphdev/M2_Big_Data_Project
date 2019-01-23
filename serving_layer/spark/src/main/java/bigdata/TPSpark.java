1843129a3637c5312ef6bf618aaf85b37353a7b9
Manon Philippot <manon.philippot@gmail.com>
2019-01-23 03:24:34 +0100

    Spark pre-processing + slice tiles into subtiles + coloration

--- serving_layer/spark/src/main/java/bigdata/TPSpark.java
+++ serving_layer/spark/src/main/java/bigdata/TPSpark.java
@@ -1,15 +1,41 @@
 package bigdata;
-
+import java.io.File;
+import javax.imageio.ImageIO;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.input.PortableDataStream;
+import scala.Tuple2;
 
 public class TPSpark {
 
 	public static void main(String[] args) {
 		
-		SparkConf conf = new SparkConf().setAppName("TP Spark");
+		SparkConf conf = new SparkConf().setAppName("Spark");
 		JavaSparkContext context = new JavaSparkContext(conf);
 		
+		// input path
+		String inputPath = "/user/lleduc/hgt/";
+		
+		// import files into a PairRDD <filename, stream>
+		JavaPairRDD<String, PortableDataStream> importedRDD = context.binaryFiles(inputPath);
+		
+		// now we convert the bytes stream into shorts
+		JavaPairRDD<Tuple2<Integer, Integer>, short[]> processedRDD = importedRDD.mapToPair(Utils.mapBytesToShorts);
+		
+		// and we correct the values that are not correct (negative)
+		JavaPairRDD<Tuple2<Integer, Integer>, short[]> correctedRDD = processedRDD.mapToPair(Utils.correctValues);
+		
+		// from all the short[], we get a new RDD with all the sub-images of size 256*256, in byte[]
+		JavaPairRDD<String, byte[]> subImagesRDD = correctedRDD.flatMapToPair(Utils.to256SizedTiles);
+		
+		// we combine the sub-images with the same key, to get the full sub-images and get rid of the fragments
+		JavaPairRDD<String, byte[]> subImagesCombinedRDD = subImagesRDD.reduceByKey(Utils.combineImagesWithSameKey);
+				
+		subImagesCombinedRDD.foreach(file -> {
+			ImageIO.write(Utils.byteStreamToBufferedImage(file._2), "png", new File(file._1 + ".png"));
+		});
+				
+		context.close();	
 	}
-	
 }
---
+++ serving_layer/spark/src/main/java/bigdata/Utils.java
@@ -0,0 +1,242 @@
+package bigdata;
+
+import java.awt.image.BufferedImage;
+import java.awt.Graphics;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import javax.imageio.ImageIO;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFlatMapFunction;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.input.PortableDataStream;
+import scala.Tuple2;
+
+public class Utils {
+	
+	public static final int tileSize = 1201;
+	public static final int smallTileSize = 256;
+	public static final int halfNumberTilesLong = 90;
+	public static final int halfNumberTilesLat = 180;
+
+	// convert PortableDataStream to short[]
+	public static PairFunction<Tuple2<String, PortableDataStream>, Tuple2<Integer, Integer>, short[]> mapBytesToShorts = new PairFunction<Tuple2<String, PortableDataStream>, Tuple2<Integer, Integer>, short[]>() {
+		
+		private static final long serialVersionUID = -6350258633341747009L;
+
+		public Tuple2<Tuple2<Integer, Integer>, short[]> call(Tuple2<String, PortableDataStream> file) throws Exception {
+			Tuple2<Integer, Integer> tileCoords = getTileCoords(file._1);
+			short[] content = convertByteArrayToShortArray(file._2.toArray());
+			
+			return new Tuple2<Tuple2<Integer, Integer>, short[]>(tileCoords, content);
+		}
+
+	};
+	
+	private static Tuple2<Integer, Integer> getTileCoords (String path) {
+		String fullFileName = path.substring(path.lastIndexOf("/")+1);
+		String fileName = fullFileName.substring(0, fullFileName.lastIndexOf("."));
+		    
+        String longLabel = fileName.substring(0, 1).toLowerCase();
+		Integer longValue = Integer.parseInt(fileName.substring(1, 3));
+		
+		String latLabel = fileName.substring(3, 4).toLowerCase();
+		Integer latValue = Integer.parseInt(fileName.substring(4, 7));
+		
+		if (longLabel.equals("n")) {
+			longValue = halfNumberTilesLong - longValue;
+		} else if (longLabel.equals("s")) {
+			longValue = halfNumberTilesLong + longValue - 1;
+		}
+		
+		if (latLabel.equals("w")) {
+			latValue = halfNumberTilesLat - latValue;
+		} else if (latLabel.equals("e")) {
+			latValue = halfNumberTilesLat + latValue - 1;
+		}
+		
+		return new Tuple2<Integer, Integer>(longValue, latValue);
+	}
+	
+	private static short[] convertByteArrayToShortArray (byte[] bytes) {
+		short[] shorts = new short[tileSize * tileSize];
+		ByteBuffer.wrap(bytes).order(ByteOrder.BIG_ENDIAN).asShortBuffer().get(shorts);
+		return shorts;
+	}
+	
+	// correct values if < 0 or > 255
+	public static PairFunction<Tuple2<Tuple2<Integer, Integer>, short[]>, Tuple2<Integer, Integer>, short[]> correctValues = new PairFunction<Tuple2<Tuple2<Integer, Integer>, short[]>, Tuple2<Integer, Integer>, short[]>() {
+		
+		private static final long serialVersionUID = 1337462003487647478L;
+
+		public Tuple2<Tuple2<Integer, Integer>, short[]> call(Tuple2<Tuple2<Integer, Integer>, short[]> file) throws Exception {
+			short[] tab = file._2;
+			for (int i = 0; i < tab.length; i++){
+				if (tab[i] < 0)
+					tab[i] = 0;
+				if (tab[i] > 255)
+					tab[i] = 255;
+			}
+			return new Tuple2<Tuple2<Integer, Integer>, short[]>(file._1, tab);
+		}
+	};
+
+	// get RDD with all 256*256 subImages from tiles, in byte[]
+	public static PairFlatMapFunction<Tuple2<Tuple2<Integer, Integer>, short[]>, String, byte[]> to256SizedTiles = new PairFlatMapFunction<Tuple2<Tuple2<Integer, Integer>, short[]>, String, byte[]>() {
+
+		private static final long serialVersionUID = 6295049499770038007L;
+		public static final int minTgtPerTile = 5;
+		public static final int minSizeTmpImage = 1280;
+		public static final int maxTgtPerTile = 6;
+		public static final int maxSizeTmpImage = 1536;
+
+		@Override
+		public Iterator<Tuple2<String, byte[]>> call(Tuple2<Tuple2<Integer, Integer>, short[]> file)
+				throws Exception {
+			
+			List<Tuple2<String, byte[]>> res = new ArrayList<Tuple2<String, byte[]>>();
+			
+			// all the coordinates useful for later
+			int yTile = file._1._1;
+			int xTile = file._1._2;
+			
+			int yOriginInBigImage = yTile * tileSize;
+			int xOriginInBigImage = xTile * tileSize;
+			
+			int y_min = yOriginInBigImage % smallTileSize;
+			int x_min = xOriginInBigImage % smallTileSize;
+			
+			int y_max = tileSize + y_min;
+			int x_max = tileSize + x_min;
+			
+			Tuple2<Integer, Integer> tmpImgY = y_max <= (5 * smallTileSize)? new Tuple2<Integer, Integer>(minTgtPerTile, minSizeTmpImage): new Tuple2<Integer, Integer>(maxTgtPerTile, maxSizeTmpImage);
+			Tuple2<Integer, Integer> tmpImgX = x_max <= (5 * smallTileSize)? new Tuple2<Integer, Integer>(minTgtPerTile, minSizeTmpImage): new Tuple2<Integer, Integer>(maxTgtPerTile, maxSizeTmpImage);
+
+			// here we start the big work
+			BufferedImage image = new BufferedImage(tmpImgX._2, tmpImgY._2, BufferedImage.TYPE_INT_ARGB);
+			
+			short[] heights = file._2;
+			int a = 0;
+			
+			for (int j = y_min; j < y_max; j++) {
+				for (int i = x_min; i < x_max; i++) {
+					image.setRGB(i, j, getColor(heights[a]));
+					a++;
+				}
+			}
+			
+			for (int j = 0; j < tmpImgY._1; j++){
+				for (int i = 0; i < tmpImgX._1; i++) {
+					int indexY = yOriginInBigImage / smallTileSize + j;
+					int indexX = xOriginInBigImage / smallTileSize + i;
+					BufferedImage subImage = image.getSubimage(i * smallTileSize, j * smallTileSize, smallTileSize, smallTileSize);
+					byte[] subImageInBytes = bufferedImageToByteStream(subImage);
+					res.add(new Tuple2<String, byte[]>(indexY + "-" + indexX, subImageInBytes));
+				}
+			}
+			
+			return res.iterator();
+		}
+	};
+	
+	public static final int getColor(short height) {
+		int r = 255, g = 255, b = 255;
+		if(height == 0){
+			r = 14;
+			g = 93;
+			b = 183;
+		} else if((height > 0) && (height <= 85)) {
+			r = 0;
+			g = 85 + height*2;
+			b = 85;
+		} else if(height > 85 && height <= 170) {
+			r = 85 + height * 2;
+			g = 255;
+			b = 85;
+		}else if(height > 170 && height <= 254) {
+			r = 220 - height;
+			g = 220 - height;
+			b = 110 - height;
+		}else if(height == 255) {
+			r = 80;
+			g = 80;
+			b = 10;
+		}
+		return (255<<24) | (r<<16) | (g<<8) | b;
+	}
+	
+	public static final byte[] bufferedImageToByteStream(BufferedImage image) {
+		byte[] imageInByte = null;
+		try {
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			ImageIO.write(image, "png", baos);
+			baos.flush();
+			imageInByte = baos.toByteArray();
+			baos.close();
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+		return imageInByte;
+	}
+	
+	public static final BufferedImage byteStreamToBufferedImage(byte[] stream) {
+		ByteArrayInputStream bais = new ByteArrayInputStream(stream);
+		BufferedImage subImage = null;
+		try {
+			subImage = ImageIO.read(bais);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return subImage;
+	}
+	
+	// we combine all the images with the same key
+	public static Function2<byte[], byte[], byte[]> combineImagesWithSameKey = new Function2<byte[], byte[], byte[]>() {
+
+		private static final long serialVersionUID = 2044592339629795644L;
+
+		@Override
+		public byte[] call(byte[] oneBytes, byte[] twoBytes) throws Exception {
+			BufferedImage oneImage = byteStreamToBufferedImage(oneBytes);
+			BufferedImage twoImage = byteStreamToBufferedImage(twoBytes);
+			
+			
+			BufferedImage combinedImage = new BufferedImage(smallTileSize, smallTileSize, BufferedImage.TYPE_INT_ARGB);
+			
+			Graphics g = combinedImage.getGraphics();
+			g.drawImage(oneImage, 0, 0, null);
+			g.drawImage(twoImage, 0, 0, null);
+			
+			return bufferedImageToByteStream(combinedImage);
+		}
+		
+	};
+	
+	// convert tile in short[] format to RGB BufferedImage
+	public static PairFunction<Tuple2<Tuple2<Integer, Integer>, short[]>, Tuple2<Integer, Integer>, BufferedImage> toBufferedImage = new PairFunction<Tuple2<Tuple2<Integer, Integer>, short[]>, Tuple2<Integer, Integer>, BufferedImage>() {
+
+		private static final long serialVersionUID = -5953046102975386641L;
+
+		@Override
+		public Tuple2<Tuple2<Integer, Integer>, BufferedImage> call(Tuple2<Tuple2<Integer, Integer>, short[]> file) throws Exception {
+			BufferedImage image = new BufferedImage(tileSize, tileSize, BufferedImage.TYPE_INT_ARGB);
+			
+			short[] heights = file._2;
+			int a = 0;
+			
+			for (int j = 0; j < tileSize; j++) {
+				for (int i = 0; i < tileSize; i++) {
+					image.setRGB(i, j, getColor(heights[a]));
+					a++;
+				}
+			}
+
+			return new Tuple2<Tuple2<Integer, Integer>, BufferedImage>(file._1, image);
+		}
+	};
+}
